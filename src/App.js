62ddacd51a96fa47e533dda8ba37723fa682c4cf
sareenakul <sareen.akul@gmail.com>
2023-11-11 03:19:02 -0500

    The Navbar is almost done, just got to do some spacing for the logo.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
 import Main from './components/Main';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,9 @@ import React from "react"
 import home from "../Assets/img/vids/home.mp4"
 const Main = ()=>{
     return(
-        <div className="main">
-            <video src={home} autoPlay loop/>
+        <div className="main" style={{ width: '100%', height: 'auto' }}>
+            <div className="overlay"></div>
+            <video src={home} autoPlay loop muted style={{ width: '100%', height: 'auto' }}/>
             <div className="content">
                 <h1>Medley Diaries</h1>
             </div>
