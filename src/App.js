f7644376eea422e15a2e4fd6ce741940e8d20eb6
sareenakul <sareen.akul@gmail.com>
2023-11-13 03:11:48 -0500

    Made the banner layout and worked on the dynamic typing effect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import Main from './components/Main';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Banner } from './components/Banner';
 
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <div className="App">
       <Main/>
       <NavBar></NavBar>
+      <Banner></Banner>
+      
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,60 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import logo from "../Assets/img/logo.png";
+import { useState, useEffect } from "react";
+
+export const Banner =()=>{
+    const [loopNum, setLoopNum] = useState(0);
+    const [isRemoved, setIsRemoved] = useState(false);
+    const rotate = [" Vlogger", "n Editor", " Developer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    const period = 2000;
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            tick();
+        }, delta)
+        return()=>{clearInterval(ticker)};
+    }, [text])
+
+    const tick = ()=>{
+        let i = loopNum % rotate.length;
+        let fullText = rotate[i];
+        let updatedText = isRemoved ? fullText.substring(0, text.length-1): fullText.substring(0, text.length+1);
+        setText(updatedText);
+
+        if(isRemoved){
+            setDelta(prevDelta => prevDelta/2)
+        }
+
+        if(!isRemoved && updatedText === fullText){
+            setIsRemoved(true);
+            setDelta(period);
+        } else if(isRemoved && updatedText === ''){
+            setIsRemoved(false);
+            setLoopNum(loopNum + 1);
+            setDelta(100);
+        }
+
+    }
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to MedleyDiaries</span>
+                        <h1>{`hi, I am a`}
+                        <span className="wrap">{text}</span>
+                        </h1>
+                        <p>MedleyDiaries has kept a record of the most amazing days of mine here in Canada</p>
+                        <button onClick={()=> console.log('connect')}>Let's Connect<ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={logo} alt="Header"/>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
