abc0afd631c67d4a32ecb4a789bed42b2a1cba45
sareenakul <sareen.akul@gmail.com>
2023-11-16 00:15:06 -0500

    Added the Skill component in my project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from './components/NavBar';
 import Main from './components/Main';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Banner } from './components/Banner';
+import Skills from './components/Skills';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
       <Main />
     </div>
   );
---
+++ src/components/Skills.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+// Correct the export statement
+const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>This is my dummy text for skills</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+// Export the component correctly
+export default Skills;
