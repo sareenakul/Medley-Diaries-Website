88da50416dfb9bbc07c008d5f22e97e368ff3991
sareenakul <sareen.akul@gmail.com>
2023-12-11 20:21:53 -0500

    Nothing much

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,35 +1,32 @@
 import { Col } from "react-bootstrap"
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 
 const ProjectCard = ({title, description, url, onVideoPlay}) =>{
+    const videoRef = useRef(null);
     const [isPlaying, setIsPlaying] = useState(false);
 
+    const handleVideoEnd = () => {
+        // Reset the video when it reaches the end
+        if (videoRef.current) {
+          videoRef.current.pause();
+          videoRef.current.currentTime = 0;
+        }
+        setIsPlaying(false);
+      };
 
-const opts = {
-    height: "390",
-    width: "640",
-    playerVars: {
-        autoplay: 0,
-    },
-}
-
-const onPlay = () => {
-    setIsPlaying(true);
-  };
-
-  const onPause = () => {
-    setIsPlaying(false);
-  };
-
-  const onEnd = () => {
-    setIsPlaying(false);
-  };
+      const handleClick = () => {
+        if (!isPlaying) {
+          onVideoPlay(videoRef); // Notify the parent component about the currently playing video
+        }
+        setIsPlaying(!isPlaying);
+      };
+  
     return(
-        <Col sm={6} md={4}>
+        <Col sm={6} md={4} onClick={handleClick}>
             <div className={`proj-imgbx ${isPlaying ? 'playing' : ''}`}>
             <div className="ratio ratio-16x9">
-                <iframe src={url} title={title} opts={opts} onPlay={onPlay} onPause={onPause} onEnd={onEnd} allowFullScreen></iframe>
+                <iframe src={url} title={title} width="640" height="390" frameBorder="0" allowFullScreen onClick={handleClick} onEnded={handleVideoEnd} ref={videoRef}></iframe>
             </div>
                 <div className="proj-txtx">
                     <h4>{title}</h4>
