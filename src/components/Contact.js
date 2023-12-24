feae61795f01517b08712561288c097d683a4f6f
sareenakul <sareen.akul@gmail.com>
2023-12-23 23:34:25 -0500

    Integrated with Emailjs and changed Contact Styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,68 +1,74 @@
-import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import logo from "../Assets/img/logo.png"
-
-const Contact = () =>{
-    const formInitialData = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: '',
-    }
-
-    const [formData, setFormData] = useState(formInitialData);
-    const [submitButtonText, setSubmitButtonText] = useState('Send');
-    const [status, setStatus] = useState({});
-    const onFormUpdate = (category, value) =>{
-        setFormData({
-            ...formData,
-            [category]: value
-        })
-    }
-    const handleSubmit = () =>{}
-    return(
-        <section className="contact" id="connect">
-            <Container>
-                <Row className="align-items-center">
-                    <Col md={6}>
-                        <img src={logo} alt="Contact Us"/>
-                    </Col>
-                    <Col md={6}>
-                        <h2>Get in Touch!</h2>
-                        <form onSubmit={handleSubmit}>
-                            <Row>
-                                <Col sm={6} className="px-1">
-                                    <input type="text" value={formData.firstName} placeholder="First Name" onChange={(e)=> onFormUpdate('firstName', e.target.value )}/>
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="text" value={formData.lastName} placeholder="Family Name" onChange={(e)=> onFormUpdate('lastName', e.target.value )}/>
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="email" value={formData.email} placeholder="Mail ID" onChange={(e)=> onFormUpdate('email', e.target.value )}/>
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="tel" value={formData.phone} placeholder="Contact Number" onChange={(e)=> onFormUpdate('phone', e.target.value )}/>
-                                </Col>
-                                <Col>
-                                    <textarea rows="6" value={formData.message} placeholder="Message" onChange={(e)=> onFormUpdate('message', e.target.value )}/>
-                                    <button type="submit">
-                                        <span>{submitButtonText}</span>
-                                    </button>
-                                </Col>
-                                {
-                                    status.message &&
-                                    <Col>
-                                        <p className={status.success === false? "Warning" : "Success"}>{status.message}</p>
-                                    </Col>
-                                }
-                            </Row>
-                        </form>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+import React, { createRef } from 'react';
+import emailjs from '@emailjs/browser';
+import logo from "../Assets/img/logo.png";
+const isValidEmail = (email) => {
+    // Regular expression for a basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+class ContactUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.form = createRef();
+    this.state = {
+        submissionStatus: null,
+    };
+  }
+  sendEmail = (e) => {
+    e.preventDefault();
+    const userEmail = this.form.current.elements.user_email.value;
+    if (!isValidEmail(userEmail)) {
+        // Display an error message or take appropriate action
+        console.log('Invalid email address');
+        this.setState({ submissionStatus: 'error' });
+        alert('Invalid email address. Please enter a valid email.');
+        return;
+      }
+    emailjs.sendForm('service_w4mgxgg', 'template_v5h2hih', this.form.current, 'nZH4gydG6dRtw1G8m')
+      .then((result) => {
+        this.setState({ submissionStatus: 'success' });
+        this.resetForm();
+        alert('Message sent successfully!');
+      }, (error) => {
+        this.setState({ submissionStatus: 'error' });
+        alert('Something went wrong. Please try again later.');
+      });
+  };
+  resetForm = () => {
+    // Reset the form fields
+    this.form.current.reset();
+  };
+  render() {
+    const { submissionStatus } = this.state;
+    return (
+      <div className="contact-container">
+        <div className="form-copy-box">
+            <div className="logo-box">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+        </div>
+        <div className="form-box">
+            <div className="header">
+                <h2>Let's Connect</h2>
+            </div>
+          <form ref={this.form} onSubmit={this.sendEmail} className="form">
+            <label>Name</label>
+            <input type="text" name="from_name" className="input-field"/>
+            <label>Email</label>
+            <input type="email" name="user_email" className="input-field"/>
+            <label>Message</label>
+            <textarea name="message" className="input-field"/>
+            <input type="submit" value="Send" className="submit-button" />
+          </form>
+          {submissionStatus === 'success' && (
+            <p className="success-message">Message sent successfully!</p>
+          )}
+          {submissionStatus === 'error' && (
+            <p className="error-message">Something went wrong. Please try again later.</p>
+          )}
+        </div>
+      </div>
     );
+  }
 }
-
-export default Contact;
+export default ContactUs;
